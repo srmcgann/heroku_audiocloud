4358156492d9fc613ee9a4ca3d7310bd420953a7
scott mcgann <s.r.mcgann@hotmail.com>
2023-05-23 19:35:52 +0000

    sync

--- fetchUserData.php
+++ fetchUserData.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 require('db.php');
   $data = json_decode(file_get_contents('php://input'));
   $userID = (int)mysqli_real_escape_string($link, $data->{'userID'});
