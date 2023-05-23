f9d3a0f95fa5d7274e8e263dcc46f4620679c3af
scott mcgann <s.r.mcgann@hotmail.com>
2023-05-23 18:37:18 +0000

    sync

--- db.php
+++ db.php
@@ -24,15 +24,5 @@ $db_ = substr($url["path"], 1);
   $baseFullURL= ($local ? 'http://' : 'https://') . $baseURL;
   $link = mysqli_connect($db_host, $db_user, $db_pass, $db);
 
-  $in = explode("\n", file_get_contents('/etc/group'));
-  foreach($in as $group){
-    $group = mysqli_real_escape_string($link, explode(':', $group)[0]);
-    $sql = 'SELECT * FROM users WHERE name LIKE "' . $group . '"';
-    $res = mysqli_query($link, $sql);
-    if(!mysqli_num_rows($res)){
-      $sql = 'INSERT INTO users (name, passhash, enabled, escaped_name) VALUES("'.$group.'", "", FALSE, "'.$group.'")';
-      mysqli_query($link, $sql);
-    }
-  }
 ?>
 
