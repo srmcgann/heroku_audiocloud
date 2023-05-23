4db92e60bcd5f56bb5f043c337ff66086e14ae4f
scott mcgann <s.r.mcgann@hotmail.com>
2023-05-23 18:29:35 +0000

    sync

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-  function alphaToDec($val){
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function alphaToDec($val){
     $pow=0;
     $res=0;
     while($val!=""){
